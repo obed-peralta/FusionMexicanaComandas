47ef15b42aace573b40cc7709c4e0439bcf4391f
Obed Peralta <adanobedperaltarojas@gmail.com>
2020-11-25 10:57:30 -0600

    Vista login escritorio
    Login en escritorio funciona!

--- controller/login.js
+++ controller/login.js
@@ -3,5 +3,14 @@ $(document).ready(function () {
 
 	console.log("login.js is working");
 
+	$('form').submit(function(e){
+		e.preventDefault();
+
+		if ($('#user').val() && $('#user').val()) {
+			console.log('Login');
+		}
+		
+	});
+
 
 });
