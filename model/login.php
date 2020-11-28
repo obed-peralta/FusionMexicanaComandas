a4756475a70b96fe0d6410b5b72fbd1d55b5b7a0
Obed Peralta <adanobedperaltarojas@gmail.com>
2020-11-28 16:31:59 -0600

    Más pulida la extracción de información
    Se encontró un error de valores en cadena y se resolvió

--- model/login.php
+++ model/login.php
@@ -12,9 +12,11 @@
 		if (!$result) {
 			echo "NONE";
 		}else{
+			
 			$row = mysqli_fetch_array($result);
+			$id = $row['id_usuario'];
 
-			$query = "SELECT c.nombre AS cargo FROM cargos c INNER JOIN detalle_usuarios du ON du.id_cargo = c.id_cargo INNER JOIN usuarios u ON u.id_detalle_usuario = du.id_detalle_usuario WHERE nombre_usuario='$row['id_usuario']'";
+			$query = "SELECT c.nombre AS cargo FROM cargos c INNER JOIN detalle_usuarios du ON du.id_cargo = c.id_cargo INNER JOIN usuarios u ON u.id_detalle_usuario = du.id_detalle_usuario WHERE nombre_usuario='$id'";
 			
 			$result = mysqli_query($connection, $query);
 			$row = mysqli_fetch_array($result);
