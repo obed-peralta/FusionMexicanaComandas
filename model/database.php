b203b02ce529b91ce16eb5b1dcc0516487314c1f
Obed Peralta <adanobedperaltarojas@gmail.com>
2020-11-29 22:59:30 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,18 +1,12 @@
 <?php
-	/*$connection = mysqli_connect(
+
+	$connection = mysqli_connect(
 		'localhost', //Host
         'root', //USer
         '', //Password
         'fusionmexicana'    // Database
-    );*/
-    $usuario='uvsqhyvwxzzvjf8t';
-    $password='RATYvqXfzM9YJif3ed9E';
-    try{
-        $connection= new PDO('mysql:host=b4nr5ofy10tdhdxpffcf-mysql.services.clever-cloud.com;dbname=b4nr5ofy10tdhdxpffcf',$usuario,$password);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e){
-        echo "Connection failed: ". $e->getMessage();
-    }
+	);
+
 	/*if ($connection) {
 
 	}*/
