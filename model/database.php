b0d1d1866f8cf94151ee58d6afd9aa2e57deb120
Adrian <ad24.ra12@gmail.com>
2020-11-29 18:09:35 -0600

    Added new directories, modified estilosLogin.css to make it more compatible with all browsers, created a function inside databse.php which returns the connection, and tried to connect it with login.php, not workin though

--- model/database.php
+++ model/database.php
@@ -1,18 +1,17 @@
 <?php
-	/*$connection = mysqli_connect(
-		'localhost', //Host
-        'root', //USer
-        '', //Password
-        'fusionmexicana'    // Database
-    );*/
-    $usuario='uvsqhyvwxzzvjf8t';
+function getDB(){
+    $user='uvsqhyvwxzzvjf8t';
     $password='RATYvqXfzM9YJif3ed9E';
+    $host='b4nr5ofy10tdhdxpffcf-mysql.services.clever-cloud.com';
+    $dbName='b4nr5ofy10tdhdxpffcf';
     try{
-        $connection= new PDO('mysql:host=b4nr5ofy10tdhdxpffcf-mysql.services.clever-cloud.com;dbname=b4nr5ofy10tdhdxpffcf',$usuario,$password);
+        $connection= new PDO('mysql:host='.$host.';dbname='.$dbName.';',$usuario,$password);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         echo "Connection failed: ". $e->getMessage();
     }
+    return $connection;
+}
 	/*if ($connection) {
 
 	}*/
--- model/login.php
+++ model/login.php
@@ -1,11 +1,10 @@
 <?php
-
 	include('database.php');
 	if (isset($_POST['user'])) {
 		$user = $_POST['user'];
 		$password = $_POST['password'];
         echo $user;
         echo $password;
-	}
-
+        $sql="SELECT id_cargo, nombre, descripcion FROM cargos;"
+    }
 ?>
