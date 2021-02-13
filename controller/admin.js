d69fa7c6bcb6149bdaaf4ce04aef179bda008dbe
obed-peralta <adanobedperaltarojas@gmail.com>
2021-02-13 09:28:38 -0600

    Mostrar Informacion del Administrador

--- controller/admin.js
+++ controller/admin.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
     console.log("admin.js it works");
     
     /**
-     * INICIO Bloque de información del usuario
+     * INICIO Bloque de acceso e información del usuario
      */
     if(window.sessionStorage){
         if(sessionStorage.getItem('id_usuario')){
             let nombre_usuario = sessionStorage.getItem('nombre_usuario');
+            $('h2').append(sessionStorage.getItem('nombre_usuario'));
             $('.drawer-title').html(`${nombre_usuario}`);
+            getUserInfo(sessionStorage.getItem('id_usuario'));
             animacionInicio();
         }else{
             $(window).attr('location','../index.html');
@@ -16,13 +18,30 @@ $(document).ready(function(){
     }else{
         throw new Error('Tu navegador web no soporta sesiones');
     }
-
+    function getUserInfo(id){
+        $.ajax({
+            url: '../model/get_user_info.php',
+            type: 'POST',
+            data: {id},
+            success: function(response){
+                let info = JSON.parse(response);
+				//console.log(info);
+				$('#nombre').html(`<label><strong>Nombre</strong></label><br><p>${info.nombre} ${info.apellidos}</p>`);
+				$('#numero_telefonico').html(`<label><strong>Teléfono</strong><p>${info.telefono}</p>`);
+				$('#email').html(`<label><strong>Email</strong><p>${info.email}</p>`);
+            }
+        });
+    }
     $('#drawer-demo').drawer({
 		//backdrop: 'static',
 		//keyboard: false,
 		focus: false,
 		show: false
 	});
+
+    /**
+     * FIN Bloque de acceso e información del usuario
+     */
 
     /**
      * INICIO Bloque para generar animación de circulos
@@ -62,6 +81,7 @@ $(document).ready(function(){
         window.sessionStorage.removeItem('id_usuario');
         window.sessionStorage.removeItem('nombre_usuario');
         window.sessionStorage.removeItem('id_cargo');
+        window.sessionStorage.removeItem('id_detalle_usuario');
         $(window).attr('location','../index.html');
      });
 
